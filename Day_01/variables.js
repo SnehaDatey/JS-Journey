bca5ce2bb4696da513c947328116974694ce73dc
Sneha Datey <snehadatey@gmail.com>
2026-08-07 11:38:45 +0530

    Day 2 practise

--- Day_01/variables.js
+++ Day_01/variables.js
@@ -3,3 +3,15 @@ let age = 37;
 let city = "Luxembourg";
 
 console.log("My name is " + name + ", I am " + age + " years old and I live in " + city);
+
+
+const accountID = 123456;
+let accountEmail = "sn@gmail.com";
+var accountPass = "12345";
+
+//accountID = 22;
+
+//console.log("Account ID : "+ accountID);
+
+console.table({accountID, accountEmail, accountPass});
+
---
+++ Day_02/DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version     
+/*
+Primitive Data Types
+String
+Number
+Boolean
+Undefined - represents a variable that has been declared but has not yet been assigned a value  
+Null - represents the intentional absence of any object value   
+BigInt
+Symbol (bas introduction)
+typeof operator - returns the data type of a variable
+
+
+Arithmetic Operators
++
+-
+*
+/
+%
+**
+Assignment Operators
+=
++=
+-=
+*=
+/=
+
+*/
